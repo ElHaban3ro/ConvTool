2e0ea682e8e2cfe60f21158f7d471b78c5fe0b58
Fernando Leguizamo <79483552+ElHaban3ro@users.noreply.github.com>
2022-10-10 09:07:50 -0700

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from ConvTool import conv_video
-
-
-
-conv_video(folder = True, convert_to = 'mp4', delete_original = True, route = r'C:\Users\ferdh\Desktop\Projects\Youtube\Global Resources\Analog Video Package\Assets\Textures (Glitches)')
